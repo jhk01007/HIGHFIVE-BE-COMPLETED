c8952fd01dd61d373cbeec98f3f0a215d2fc85c4
koungq <guu119@naver.com>
2025-04-08 15:17:49 +0900

    Feat: Setting Ai Prompt Server

---
+++ ai-engine-service/src/test/java/advancedweb/project/aiengineservice/AiEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package advancedweb.project.aiengineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
