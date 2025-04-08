207dedbff105b27694f0e5dcb0717ae0ef48867e
koungq <guu119@naver.com>
2025-04-08 16:59:25 +0900

    Feat: Eureka and DB Setting

--- user-service/src/main/java/advancedweb/project/userservice/domain/entity/User.java
+++ user-service/src/main/java/advancedweb/project/userservice/domain/entity/User.java
@@ -1,4 +1,13 @@
 package advancedweb.project.userservice.domain.entity;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User {
+
+    private String userNo;      // PK
+    private String nickname;    // 닉네임
+    private String username;    // 로그인 아이디
+    private String password;    // 로그인 비밀번호
+
 }
---
+++ welfare-service/src/main/java/advancedweb/project/welfareservice/domain/entity/WelfareType.java
@@ -0,0 +1,6 @@
+package advancedweb.project.welfareservice.domain.entity;
+
+public enum WelfareType {
+
+    // 복지 서비스 필터링 정보
+}
