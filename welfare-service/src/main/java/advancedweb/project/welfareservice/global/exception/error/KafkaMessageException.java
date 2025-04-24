3b25e56e9871acc6d768a83105b5d6da82b50b3c
koungq <guu119@naver.com>
2025-04-24 15:33:52 +0900

    feat: global exception handler

--- welfare-service/src/main/java/advancedweb/project/welfareservice/global/exception/error/KafkaMessageException.java
+++
@@ -1,7 +0,0 @@
-package advancedweb.project.welfareservice.global.exception.error;
-
-public class KafkaMessageException extends RuntimeException {
-    public KafkaMessageException() {
-        super("Kafka 메세지 전송 오류");
-    }
-}
