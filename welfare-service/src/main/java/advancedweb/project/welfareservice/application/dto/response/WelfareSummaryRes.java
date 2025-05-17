6147382df5ef09c8497eef7fe1caeac1e1c92d56
koungq <guu119@naver.com>
2025-05-17 20:54:17 +0900

    feat: recommend welfare to llm

--- welfare-service/src/main/java/advancedweb/project/welfareservice/application/dto/response/WelfareSummaryRes.java
+++ welfare-service/src/main/java/advancedweb/project/welfareservice/application/dto/response/WelfareSummaryRes.java
@@ -3,9 +3,13 @@ package advancedweb.project.welfareservice.application.dto.response;
 import advancedweb.project.welfareservice.domain.entity.Welfare;
 
 public record WelfareSummaryRes (
+        String welfareNo,
         String title
 ) {
     public static WelfareSummaryRes create(Welfare welfare) {
-        return new WelfareSummaryRes(welfare.getSummary().getName());
+        return new WelfareSummaryRes(
+                welfare.getWelfareNo(),
+                welfare.getSummary().getName()
+        );
     }
 }
